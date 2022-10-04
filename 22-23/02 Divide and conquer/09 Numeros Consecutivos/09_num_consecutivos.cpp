44957fef015c2b5929156e85a892cceda8cf9db0
Amph Rubio Bellón <ampxrubio@gmail.com>
2022-10-04 10:59:15 +0200

    2-09 resuelto

---
+++ 22-23/02 Divide and conquer/09 Numeros Consecutivos/09_num_consecutivos.cpp
@@ -0,0 +1,61 @@
+// Amparo Rubio Bellon
+// VJ54
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+// O(n)
+int resolver(const std::vector<int>&v,int ini, int fin) {
+    if(fin - ini==0){ //caso base
+        return v[ini];
+    }
+    
+    if(v[ini+1] - v[ini]!=1){ //si no es consecutivo ya termina la busqueda
+        return v[ini];
+    }
+    else{
+        return resolver(v,ini +1,fin); //si es avanzamos
+    }
+    
+}
+
+
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int n;
+    std::cin>>n;
+    std::vector<int>v(n);
+    
+    for(int i=0;i<n;i++)
+        std::cin>>v[i];
+    
+    // escribir sol
+    std::cout<<resolver(v,0,n-1)<<"\n";
+    
+}
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+    
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
+}
