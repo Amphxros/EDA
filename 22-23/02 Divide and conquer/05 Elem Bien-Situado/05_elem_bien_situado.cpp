7b4c16f228233e2784e8cd1f7d090205e0236129
Amph Rubio Bellón <ampxrubio@gmail.com>
2023-05-24 15:14:07 +0200

    2 05 hecho

--- 22-23/02 Divide and conquer/05 Elem Bien-Situado/05_elem_bien_situado.cpp
+++ 22-23/02 Divide and conquer/05 Elem Bien-Situado/05_elem_bien_situado.cpp
@@ -1,47 +1,74 @@
 // Amparo Rubio Bellon
-// VJ 54 
+// VJ54
 
 
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-
 #include <vector>
 
-bool elemento_situado(const std::vector <int>& v, int ini, int fin) { // o(log n)
-	if (fin - ini > 1) {
-		int mit = fin + ini / 2;
-		if (v[mit] > mit) {
-			return elemento_situado(v, ini, mit);
-		}
-		else if (v[mit] < mit) {
-			return elemento_situado(v, mit, fin);
-		}
-		else return true;
+
+// O(log(n)) puesto que el vector está ordenado
+bool resolver(std::vector<int> v, int ini, int fin) {
+
+	if(fin-ini==0){
+		return v[ini]==ini;
 	}
-	else if (fin - ini == 1) return v[ini] == ini;
-	return false;
-}
+	else{
+		int mit= (ini+fin)/2;
+		if(v[mit]==mit){
+			return true;
+		}
+		else if(v[mit]>mit){
+			return resolver(v,ini,mit-1);
+		}
+		else{
+			return resolver(v,mit+1,fin);
+		}
+	}
     
-	
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+	int n;
+	std::cin >> n;
+	std::vector <int> v(n);
+	for(int i = 0; i < n; i++){
+		std::cin >> v[i];
+	}
+
+	if(resolver(v,0,n-1)){
+		std::cout << "SI" << "\n";
+	}
+	else{
+		std::cout << "NO" << "\n";
+	}
+    
+}
 
 int main() {
-	int casosPrueba = 0;
-	std::cin >> casosPrueba;
-	for (int i = 0; i < casosPrueba; i++) {
-		std::vector<int> v;
-	    int cant = 0;
-	    std::cin >> cant;
-	    for (int i = 0; i < cant; i++) {
-		    int n;
-		    std::cin >> n;
-		    v.push_back(n);
-	}
-    	if (elemento_situado(v, 0, v.size())) //o(log n)
-			std::cout << "SI" << "\n";
-		else 
-            std::cout << "NO" << "\n";
-	}
-	return 0;
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+    
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
 }
-	
